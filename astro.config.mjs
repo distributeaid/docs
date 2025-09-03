8b7f1c52a4e8fb6c5d27e55be74551d2c3b80371
jtfairbank <jtfairban@gmail.com>
2025-09-04 01:39:00 +0200

    feat: serve the site from the main directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://distributeaid.github.io',
-	base: '/docs',
 	integrations: [
 		starlight({
 			title: 'My Docs',
